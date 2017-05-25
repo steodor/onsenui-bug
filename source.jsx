229f3db0a92d096ea6bc5324a1b20c75e289163a
Teodor Sandu <teodor.sandu@dotnetwise.com>
2017-05-25 14:39:18 +0300

    Added bug reproduction files

---
+++ source.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ons from 'onsenui';
+import Ons from 'react-onsenui';
+
+ReactDOM.render(
+    <div>asd</div>,
+    document.getElementById('app')
+);
